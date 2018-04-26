787c77fbec211974d8206fdbbad08420a2485de1
renanmaringolo <renan.maringolo@gmail.com>
2018-04-26 09:26:54 -0300

    initial steps

---
+++ ruby-logic/maior_ou_menor.rb
@@ -0,0 +1,16 @@
+puts "Bem-vindo ao jogo da adivinhação"
+puts "Qual é o seu nome?"
+nome = gets
+puts "\n\n\n\n\n"
+puts "Começaremos o jogo para você, " + nome
+
+puts "Escolhendo um número secreto entre 0 e 200..."
+numero_secreto = 175
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+puts "\n\n\n\n\n"
+puts "Tentativa 1"
+puts "Entre com o número"
+chute = gets
+puts "Será que acertou? Você chutou " + chute
+puts chute.to_i == numero_secreto
+puts "Até a próxima!"
---
+++ ruby-logic/teste.rb
@@ -0,0 +1,9 @@
+puts 4 > 3
+puts 7 < 19
+puts 10 >= 10
+puts 20 <= 19
+puts "Qual modelo do seu celular?"
+celular = gets
+puts "Seu celular é um: " + celular
+
+puts "Em linguagem de programação, esses dois exemplos de entrada e saída são muito comuns."
