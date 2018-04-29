dfd2b0aed5eca81965ff21ea7e554381eb215433
renanmaringolo <renan.maringolo@gmail.com>
2018-04-29 10:09:50 -0300

    two_for

--- ruby-logic/teste.rb
+++ ruby-logic/teste.rb
@@ -1,9 +1,3 @@
-puts 4 > 3
-puts 7 < 19
-puts 10 >= 10
-puts 20 <= 19
-puts "Qual modelo do seu celular?"
-celular = gets
-puts "Seu celular é um: " + celular
-
-puts "Em linguagem de programação, esses dois exemplos de entrada e saída são muito comuns."
+for contar in 1..3
+    puts "contando " + contar.to_s
+end    
