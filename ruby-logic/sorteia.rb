e096581f22211e5e157f2f22d6b9f25e437f3d6b
renanmaringolo <renan.maringolo@gmail.com>
2018-05-02 16:26:32 -0300

    exercise_3

---
+++ ruby-logic/sorteia.rb
@@ -0,0 +1,7 @@
+def sorteia_numero_secreto
+	puts "Escolhendo um número secreto entre 0 e 200..."
+	sorteado = 175
+	puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+	sorteado
+end
+numero_secreto = sorteia_numero_secreto
---
+++ ruby-logic/welcome.rb
@@ -0,0 +1,8 @@
+def da_boas_vindas
+    puts "Bem-vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n\n\n"
+    puts "Começaremos o jogo para você, " + nome
+end
+da_boas_vindas
