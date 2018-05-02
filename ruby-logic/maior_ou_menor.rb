b22f7e7152a1eb9e96a49f5309c8fe40d9ae05cc
renanmaringolo <renan.maringolo@gmail.com>
2018-05-02 12:32:23 -0300

    encap-def

--- ruby-logic/maior_ou_menor.rb
+++ ruby-logic/maior_ou_menor.rb
@@ -1,33 +1,52 @@
-puts "Bem-vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
+def da_boas_vindas
+    puts "Bem-vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n\n\n"
+    puts "Começaremos o jogo para você, " + nome
+end
 
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
 
-limite_de_tentativas = 5
+def sorteia_numero_secreto
+    puts "Escolhendo um número secreto entre 0 e 200..."
+    sorteado = 175
+    puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+    sorteado
+end
 
-for tentativa in 1..limite_de_tentativas
-
+def pede_numero(tentativa, limite_de_tentativas)
     puts "\n\n\n\n\n"
     puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
     puts "Entre com o número"
     chute = gets
     puts "Será que acertou? Você chutou " + chute
+    chute.to_i
+end    
 
-    if numero_secreto == chute.to_i
+def verifica_se_acertou(chute, numero_secreto)
+    acertou = numero_secreto == chute.to_i
+    if acertou
         puts "Você ACERTOU!!!"
+        return true
+    end
+    maior = numero_secreto > chute.to_i
+    if maior
+        puts "O número secreto é maior..."
+    else
+        puts "O número secreto é menor..."
+    end     
+    false           
+end    
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+    chute = pede_numero(tentativa, limite_de_tentativas) 
+    if verifica_se_acertou(chute, numero_secreto)
         break
-    else   
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior..."
-        else
-            puts "O número secreto é menor..."
-        end        
-    end        
+    end 
 end
 puts "Adeus."
