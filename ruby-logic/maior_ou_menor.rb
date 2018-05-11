d676bcb031e625c5063bf2451bce91c3464ca8c5
renanmaringolo <renan.maringolo@gmail.com>
2018-05-11 12:46:42 -0300

    cheater

--- ruby-logic/maior_ou_menor.rb
+++ ruby-logic/maior_ou_menor.rb
@@ -1,15 +1,32 @@
 def da_boas_vindas
     puts "Bem-vindo ao jogo da adivinhação"
     puts "Qual é o seu nome?"
-    nome = gets
+    nome = gets.strip
     puts "\n\n\n\n\n"
     puts "Começaremos o jogo para você, #{nome}"
+    nome
 end
 
+def pede_dificuldade
+    puts "Escolha um nível de dificuldade. (1 Fácil, 5 Difícil)"
+    dificuldade = gets.to_i
+end    
 
-def sorteia_numero_secreto
-    puts "Escolhendo um número secreto entre 0 e 200..."
-    sorteado = rand(200)
+def sorteia_numero_secreto(dificuldade)
+    case dificuldade
+    when 1
+        maximo = 30
+    when 2
+        maximo = 60
+    when 3
+        maximo = 100
+    when 4
+        maximo = 150
+    else
+        maximo = 200
+    end    
+    puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+    sorteado = rand(maximo) + 1
     puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
     sorteado
 end
@@ -39,8 +56,9 @@ def verifica_se_acertou(chute, numero_secreto)
     false           
 end    
 
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
+nome = da_boas_vindas
+dificuldade = pede_dificuldade
+numero_secreto = sorteia_numero_secreto dificuldade
 
 pontos_ate_agora = 1000
 limite_de_tentativas = 5
@@ -50,6 +68,11 @@ chutes = []
 for tentativa in 1..limite_de_tentativas
     chute = pede_numero chutes, tentativa, limite_de_tentativas
     chutes << chute
+
+    if nome == "Renan"
+        puts "Você ACERTOU #{nome}!!!"
+        break
+    end
     pontos_a_perder = (chute - numero_secreto).abs / 2
     pontos_ate_agora -= pontos_a_perder
 
@@ -57,4 +80,4 @@ for tentativa in 1..limite_de_tentativas
         break
     end 
 end
-puts "Você ganhou #{pontos_ate_agora} pontos"
+puts "Você ganhou #{pontos_ate_agora} pontos, #{nome}"
