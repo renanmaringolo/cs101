889edbf287d020410d2c958be6122303b62670d0
renanmaringolo <renan.maringolo@gmail.com>
2018-05-31 14:00:25 -0300

    refactoring

--- ruby-logic/exercise_1_ II.rb
+++ ruby-logic/exercise_1_ II.rb
@@ -38,8 +38,37 @@ def joga(nome)
     pontos_ate_agora = 0
 
     while erros < 5
-        chute = pede_um_chute chutes, erros
-        chutes << chute  
+        chute = pede_um_chute chutes, erros 
+        if chutes.include? chute
+            puts "Você já chutou #{chute}"
+            next
+        end    
+        chutes << chute
+
+        chutou_uma_letra = chute.size == 1    
+        if chutou_uma_letra 
+
+            letra_procurada = chute[0]
+            total_encontrado = palavra_secreta.count letra_procurada ##sintax sugar (CRAZY!
+
+            if total_encontrado == 0
+                puts "Letra não encontrada!"
+                erros += 1
+            else
+                puts "Letra encontrada #{total_encontrado} vezes"             
+            end             
+        else
+            acertou = chute == palavra_secreta
+            if acertou
+                puts "Parabéns, você acertou!"
+                pontos_ate_agora += 100
+                break
+            else
+                puts "Que pena, você errou!"
+                pontos_ate_agora -= 30
+                erros += 1
+            end       
+        end
     end    
     puts "Você ganhou #{pontos_ate_agora} pontos."
 end
