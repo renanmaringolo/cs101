a90ae16375e7c18d3a1b04d95f507ff232549efe
renanmaringolo <renan.maringolo@gmail.com>
2018-11-01 11:36:20 -0300

    finalLesson2

---
+++ ruby-logic/consolidando_2.rb
@@ -0,0 +1,32 @@
+puts "Bem-vindo ao jogo da adivinhação"
+puts "Qual seu nome?"
+nome = gets
+puts "\n\n\n\n\n"
+puts "Começaremos o jogo para você " + nome
+puts "Escolhendo um número secreto..."
+numero_secreto = 120
+puts "Escolhido, que tal adivinhar nosso número secreto hoje?"
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+    puts "\n\n\n\n"
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    chute = gets
+    puts "Será que você acertou? Você chutou " + chute.to_s
+
+    acertou = numero_secreto == chute.to_i
+    if acertou
+        puts "Você acertou!"
+        break
+    else
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número secreto é maior"
+        else
+            puts "O número secreto é menor"  
+        end         
+    end    
+end    
+
+    
--- ruby-logic/teste.rb
+++ ruby-logic/teste.rb
@@ -1,10 +1,3 @@
-  puts 176 == 175
-  puts 175 == 175
-  puts
-  puts
-  puts
-  puts
-  puts 4 > 3 
-  puts 7 < 19 
-  puts 10 >= 10 
-  puts 20 <= 21 
+for dedos in 1..3
+  puts "contando"
+end   
