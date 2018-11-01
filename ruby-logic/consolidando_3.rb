2c6390a365dbd7e7822517cb9e694bfd0c2e3a81
renanmaringolo <renan.maringolo@gmail.com>
2018-11-01 16:56:18 -0300

    lesson 3 refactoring

--- ruby-logic/consolidando_3.rb
+++ ruby-logic/consolidando_3.rb
@@ -1,30 +1,50 @@
-puts "Bem-vindo ao jogo da adivinhação"
-puts "Qual seu nome?"
-nome = gets
-puts "\n\n\n\n\n"
-puts "Começaremos o jogo para você " + nome
-puts "Escolhendo um número secreto..."
-numero_secreto = 120
-puts "Escolhido, que tal adivinhar nosso número secreto hoje?"
+def da_boas_vindas
+    puts "Bem-vindo ao jogo da adivinhação"
+    puts "Qual seu nome?"
+    nome = gets
+    puts "\n\n\n\n\n"
+    puts "Começaremos o jogo para você " + nome
+end   
 
-limite_de_tentativas = 5
+def sorteia_numero_secreto
+    puts "Escolhendo um número secreto..."
+    sorteado = 175
+    puts "Escolhido, que tal adivinhar nosso número secreto hoje?"
+    sorteado
+end    
 
-for tentativa in 1..limite_de_tentativas
+def pede_um_numero(tentativa, limite_de_tentativas)
     puts "\n\n\n\n"
     puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
     chute = gets
     puts "Será que você acertou? Você chutou " + chute.to_s
+    chute.to_i
+end    
 
-    acertou = numero_secreto == chute.to_i
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
     if acertou
         puts "Você acertou!"
-        break
-    else
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior"
-        else
-            puts "O número secreto é menor"  
-        end         
+        return true
     end    
+    maior = numero_secreto > chute
+    if maior
+        puts "O número secreto é maior"
+    else
+        puts "O número secreto é menor"  
+            
+    end
+    false    
+end   
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+    chute = pede_um_numero tentativa, limite_de_tentativas
+    if verifica_se_acertou numero_secreto, chute 
+        break
+    end
 end        
