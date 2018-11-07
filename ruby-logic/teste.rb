60d35288bc56edcb2902fd33131875c40a29bf49
renanmaringolo <renan.maringolo@gmail.com>
2018-11-07 15:07:10 -0200

    lesson four

--- ruby-logic/teste.rb
+++ ruby-logic/teste.rb
@@ -2,6 +2,26 @@ for dedos in 1..3
   puts "contando"
 end   
 
-chutes = [176, 100, 130, 170, 150]
+chutes = []
+
+chute = 176
+tentativa = 1
+chutes[tentativa - 1] = chute
+
+chute = 180
+tentativa = 2
+chutes[tentativa - 1] = chute
+
+chute = 140
+tentativa = 3
+chutes[tentativa - 1] = chute
+
 
 puts chutes[0]
+puts chutes[1]
+puts chutes[2]
+
+
+for contador in 0..(tentativa-1)
+  puts "Chute: " + chutes[contador].to_s  
+end  
