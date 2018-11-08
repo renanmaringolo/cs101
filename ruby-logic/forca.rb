0c75c32d5f321d6e8bf62cf3db0b8bfef38f7865
renanmaringolo <renan.maringolo@gmail.com>
2018-11-08 19:27:38 -0200

    forca

--- ruby-logic/forca.rb
+++ ruby-logic/forca.rb
@@ -10,10 +10,8 @@ end
 def escolhe_palavra_secreta
     puts "Escolhendo palavra secreta..."
     palavra_secreta = "programador"
-    puts "Palavra secreta escolhida"
-    puts "\n\n\n"
-    puts "Que tal tentar adivinhar a palavra secreta?"
-    puts "A palavra secreta tem #{palavra_secreta.size} letras"
+    puts "A palavra secreta com #{palavra_secreta.size} letras"
+    palavra_secreta
 end    
 
 def nao_quer_jogar?
@@ -29,7 +27,7 @@ def pede_um_chute(chutes, erros)
     puts "Entre com uma letra ou uma palavra"
     chute = gets.strip
     puts "Será que acertou? Você chutou #{chute}."
-    chute
+    chute 
 end    
 
 def joga(nome)
@@ -39,14 +37,36 @@ def joga(nome)
     chutes = []
     pontos_ate_agora = 0
 
-
     while erros < 5
         chute = pede_um_chute chutes, erros
+        if chutes.include? chute
+            puts "Você já chutou #{chute}"
+            next
+        end   
         chutes << chute
 
-
-        #verifica 
-
+        chutou_uma_letra = chute.size == 1
+        if chutou_uma_letra
+            letra_procurada = chute[0]
+           total_encontrado = palavra_secreta.count letra_procurada
+            if total_encontrado == 0
+                puts "Letra não encontrada"
+                erros += 1
+            else 
+                puts "Letra encontrada #{total_encontrado} vezes."
+            end    
+        else
+            acertou = chute == palavra_secreta
+            if acertou
+                puts "Parabéns, você acertou!"    
+                pontos_ate_agora += 100
+                break
+            else 
+                puts "ERROUUUUUUUUU"
+                pontos_ate_agora -= 30
+                erros += 1    
+            end
+        end   
     end     
 
     puts "Você ganhou #{pontos_ate_agora} pontos."
