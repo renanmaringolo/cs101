e7434fc19a5860107ed37d9eeb6e9202c4ae6b82
renanmaringolo <renan.maringolo@gmail.com>
2019-05-07 02:43:59 -0300

    pacman

---
+++ ruby-logic/foge-foge/fogefoge.rb
@@ -0,0 +1,31 @@
+require_relative 'ui'
+
+def le_mapa(numero)
+    arquivo = "mapa#{numero}.txt"
+    texto = File.read arquivo
+    mapa = texto.split "\n"
+end
+
+def joga(nome)
+    mapa = le_mapa 1
+
+    while true
+        desenha mapa
+        direcao = pede_movimento
+    end    
+end
+
+def inicia_fogefoge
+    nome = da_boas_vindas
+    joga nome
+end
+
+def encontra_jogador(mapa)
+    caractere_do_heroi = "H"
+    mapa.each_with_index do |linha_atual, linha|
+        coluna_do_heroi = linha_atual.index caractere_do_heroi
+        if coluna_do_heroi
+
+        end
+    end
+end
