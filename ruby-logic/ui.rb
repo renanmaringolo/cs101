090aac4f66b069ad38305bcee76d67116347a288
renanmaringolo <renan.maringolo@gmail.com>
2019-05-07 00:53:07 -0300

    ui update

--- ruby-logic/ui.rb
+++ ruby-logic/ui.rb
@@ -19,7 +19,20 @@ def avisa_letra_encontrada(total_encontrado)
 end
 
 def avisa_acertou_palavra
-    puts "Parabéns, você acertou!"    
+    puts "\nParabéns, você ganhou!"
+    puts
+
+    puts "       ___________      "
+    puts "      '._==_==_=_.'     "
+    puts "      .-\\:      /-.    "
+    puts "     | (|:.     |) |    "
+    puts "      '-|:.     |-'     "
+    puts "        \\::.    /      "
+    puts "         '::. .'        "
+    puts "           ) (          "
+    puts "         _.' '._        "
+    puts "        '-------'       "
+    puts
 end
 
 def avisa_errou_palavra
@@ -31,13 +44,44 @@ def avisa_pontos(pontos_ate_agora)
 end
 
 def da_boas_vindas
-    puts "Bem vindo ao jogo da Forca."
-    puts "Digite seu nome: "
+    puts "/****************/"
+    puts "/ Jogo de Forca */"
+    puts "/****************/"
+    puts "Qual é o seu nome?"
     nome = gets.strip
-    puts "Começaremos o jogo para você #{nome}."
-    puts "\n\n\n\n"
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, #{nome}"
     nome
-end    
+end
+
+def desenha_forca(erros)
+    cabeca = "   "
+    corpo = " "
+    pernas = "   "
+    bracos = "   "
+    if erros >= 1
+        cabeca = "(_)"
+    end
+    if erros >= 2
+        bracos = " | "
+        corpo = "|"
+    end
+    if erros >= 3
+        bracos = "\\|/"
+    end
+    if erros >= 4
+        pernas = "/ \\"
+    end
+    puts "  _______      \n"
+    puts " |/      |     \n"
+    puts " |      #{cabeca} \n"
+    puts " |      #{bracos}  \n"
+    puts " |       #{corpo}  \n"
+    puts " |      #{pernas}  \n"
+    puts " |              \n"
+    puts "_|___           \n"
+    puts "\n\n"
+end
 
 def avisa_escolhendo_palavra
     puts "Escolhendo palavra secreta..."
@@ -56,6 +100,7 @@ end
 
 def cabecalho(chutes, erros, mascara)
     puts "\n\n\n\n"
+    desenha_forca erros
     puts "Palavra secreta: #{mascara}."
     puts "Erros até agora: #{erros}."
     puts "Chutes até agora: #{chutes}."
