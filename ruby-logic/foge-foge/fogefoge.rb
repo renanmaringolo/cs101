e77d53169ec08b803a33392e0583ce46c2af8a69
renanmaringolo <renan.maringolo@gmail.com>
2019-05-16 18:21:17 -0300

    ghostInclude

--- ruby-logic/foge-foge/fogefoge.rb
+++ ruby-logic/foge-foge/fogefoge.rb
@@ -8,16 +8,16 @@ end
 
 def calcula_nova_posicao(heroi, direcao)
     heroi = heroi.dup
-    case direcao
-    when "W"
-        heroi[0] -= 1
-    when "S"
-        heroi[0] += 1
-    when "A"
-        heroi[1] -= 1
-    when "D"            
-        heroi[1] += 1
-    end
+
+    movimentos = {
+        "W" => [-1, 0],
+        "S" => [+1, 0],
+        "A" => [0, -1],
+        "D" => [0, +1]
+    }
+    movimento = movimentos[direcao]
+    heroi[0] += movimento[0]
+    heroi[1] += movimento[1]
     heroi    
 end    
 
@@ -30,14 +30,35 @@ def posicao_valida?(mapa, posicao)
     if estourou_linha || estourou_coluna
         return false
     end
-    if mapa[posicao[0]][posicao[1]] == "X"
+    valor_atual = mapa[posicao[0]][posicao[1]]
+    if valor_atual == "X" || valor_atual == "F"
         return false
     end    
     true
 end
 
+def move_fantasma(mapa, linha, coluna)
+    posicao = [linha, coluna + 1]
+    if posicao_valida? mapa, posicao
+        mapa[linha][coluna] = " "
+        mapa[posicao[0]][posicao[1]] = "F"
+    end
+end
+
+def move_fantasmas(mapa)
+    caractere_do_fantasma = "F"
+    mapa.each_with_index do |linha_atual, linha|
+        linha_atual.chars.each_with_index do |caractere_atual, coluna|
+            eh_fantasma = caractere_atual == caractere_do_fantasma
+            if eh_fantasma
+                move_fantasma mapa, linha, coluna
+            end    
+        end
+    end        
+end
+
 def joga(nome)
-    mapa = le_mapa 1
+    mapa = le_mapa 2
 
     while true
         desenha mapa
@@ -50,6 +71,7 @@ def joga(nome)
         end    
         mapa[heroi[0]][heroi[1]] = " "
         mapa[nova_posicao[0]][nova_posicao [1]] = "H"
+        move_fantasmas mapa
     end    
 end
 
