7646e9e18edbea79d70612024a1ea3ec733feec0
Leandro Maríngolo <leandronsp@gmail.com>
2019-07-08 00:05:12 +0100

    initial implementation for roman numbers

--- ruby-logic/roman-numbers/lib/roman_number_converter.rb
+++ ruby-logic/roman-numbers/lib/roman_number_converter.rb
@@ -1,3 +1,15 @@
 class RomanNumberConverter
+  BASIC_NUMBERS_INDEX = {
+    1 => 'I',
+    5 => 'V',
+    10 => 'X',
+    50 => 'L',
+    100 => 'C',
+    500 => 'D',
+    1000 => 'M'
+  }
 
+  def self.convert(arabic)
+    BASIC_NUMBERS_INDEX[arabic]
+  end
 end
--- ruby-logic/roman-numbers/spec/roman_number_converter_spec.rb
+++ ruby-logic/roman-numbers/spec/roman_number_converter_spec.rb
@@ -1,5 +1,16 @@
 require_relative '../lib/roman_number_converter'
 
 describe RomanNumberConverter do
-
+  describe '.convert' do
+    it 'converts basic numbers' do
+      expect(described_class.convert(1)).to eq('I')
+      expect(described_class.convert(5)).to eq('V')
+      expect(described_class.convert(10)).to eq('X')
+      expect(described_class.convert(50)).to eq('L')
+      expect(described_class.convert(100)).to eq('C')
+      expect(described_class.convert(500)).to eq('D')
+      expect(described_class.convert(1000)).to eq('M')
+      expect(described_class.convert(6)).to eq('VI')
+    end
+  end
 end
